3735016b1ef04fc78dfe8648df096ef355655f9a
stanleyshen2003 <stanley.shen2003@gmail.com>
2025-11-30 20:31:13 +0800

    debug

--- upf_accel_pfcp_session.c
+++ upf_accel_pfcp_session.c
@@ -3,6 +3,7 @@
 #include "upf_accel.h"
 
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <time.h>
 #include <arpa/inet.h>
--- upf_accel_pfcp_session.h
+++ upf_accel_pfcp_session.h
@@ -7,5 +7,5 @@
 struct upf_accel_config; /* forward */
 
 struct pfcp_packet newPFCPEstablishmentResponse(uint8_t seq, bool s_flag, struct upf_accel_config *cfg);
-
+#define PFCP_MSG_SESSION_ESTABLISHMENT_RESPONSE  51
 #endif /* UPF_ACCEL_PFCP_SESSION_H_ */
